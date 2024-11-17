eb50e656445baae4909feaa26da828fe54d30ece
leonAsturizaga <leonasturizaga@gmail.com>
2024-11-17 16:57:24 -0300

    RAW calculadora de pagos, calculadora de adelanto de capital

--- proptech/src/App.jsx
+++ proptech/src/App.jsx
@@ -1,6 +1,8 @@
 import { Routes,Route } from "react-router-dom"
 import Home from "./pages/Home"
 import Contacto from "./pages/Contacto"
+import MortgageCalculator from "./components/MortgageCalculator"
+import EnhancedMortgageCalculator from "./components/EnhancedMortgageCalculator"
 
 function App() {
   
@@ -9,6 +11,8 @@ function App() {
     <Routes>
       <Route path="/" element={<Home/>}/>
       <Route path="/contacto" element={<Contacto/>}/>
+      <Route path="/calculator" element={<MortgageCalculator/>}/>
+      <Route path="/adelantoCapital" element={<EnhancedMortgageCalculator/>}/>
     </Routes>
     
   )
