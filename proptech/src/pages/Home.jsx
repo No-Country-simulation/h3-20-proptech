9e12f4553e64128150aac9fa26a2b8ce55e98682
jorgea-hn <jorgeahenriqueznovoa.com>
2024-11-22 18:32:38 -0500

    feat: add payments

--- proptech/src/pages/Home.jsx
+++ proptech/src/pages/Home.jsx
@@ -2,12 +2,14 @@
 import Navbar from "../components/Navbar";
 import Content from "../components/Content";
 import Footer from "../components/Footer";
+import Payments from "../components/Payments";
 
 function Home() {
   return (
     <>
       <Navbar/>
       <Content/>
+      <Payments/>
       <Footer/>
     </>
   );
