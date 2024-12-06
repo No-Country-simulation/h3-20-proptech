720612abd10f8fda78c8899abf3e86292e5ba90a
edivaldo gomes <edslavianov@gmail.com>
2024-12-06 02:30:39 +0100

    Safe issue

--- proptech/src/components/Navbar.jsx
+++ proptech/src/components/Navbar.jsx
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-import logo from "../assets/logo-white.png"; // Ensure correct logo path
+import logo from "../assets/logo-white.png";
 
 function Navbar() {
   return (
