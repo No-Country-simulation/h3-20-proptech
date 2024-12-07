3de0b8809191b6f1287f1b54c128548951441128
Juan Novas <juanignacionovas@gmail.com>
2024-12-07 10:34:50 -0300

    investments model update

--- server/investments/models.py
+++ server/investments/models.py
@@ -8,6 +8,17 @@ class Investor(models.Model):
     
     
 class Investment(models.Model):
+    TERM_OPTIONS = [
+        ('M', 'months'),
+        ('Y', 'years'),
+    ]
+    
     investor = models.ForeignKey(Investor, on_delete=models.CASCADE)
+    date = models.DateField(auto_created=True)
     amount = models.PositiveBigIntegerField()
     interest_rate = models.PositiveSmallIntegerField()
+    payments_amount = models.PositiveSmallIntegerField()
+    montly_return = models.PositiveIntegerField()
+    term = models.PositiveSmallIntegerField()
+    term_type = models.CharField(max_length=1, choices=TERM_OPTIONS)
+    anual_rate = models.PositiveSmallIntegerField()
