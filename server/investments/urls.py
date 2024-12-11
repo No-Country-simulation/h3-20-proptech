3983e479dd53527198ae6046e416b6d413016f62
Juan Novas <juanignacionovas@gmail.com>
2024-12-11 18:25:03 -0300

    fix url names

--- server/investments/urls.py
+++ server/investments/urls.py
@@ -2,10 +2,10 @@ from django.urls import path
 from .views import InvestmentDetailView, InvestmentListCreateView, ResultDetailView, ResultListCreateView
 
 urlpatterns = [
-    path('investment-detail/<pk:int>/', InvestmentDetailView.as_view(), name='investment-datail'),
+    path('investment-detail/<int:pk>/', InvestmentDetailView.as_view(), name='investment-datail-list'),
     path('investment-list-create/', InvestmentListCreateView.as_view(), name='investment-datail'),
     
 
-    path('result-detail/<pk:int>/', ResultDetailView.as_view(), name='result-datail'),
+    path('result-detail/<int:pk>/', ResultDetailView.as_view(), name='result-datail-list'),
     path('result-list-create/', ResultListCreateView.as_view(), name='result-datail'),
 ]
