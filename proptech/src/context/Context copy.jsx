7cbf8583f614fe9338f958885520721bd3cdafc4
leonAsturizaga <leonasturizaga@gmail.com>
2024-12-12 19:42:52 -0300

    files to merge

--- proptech/src/context/Context copy.jsx
+++ proptech/src/context/Context copy.jsx
@@ -11,6 +11,7 @@ const urlGlobal = "https://h3-20-proptech-production.up.railway.app/api/";
 export const ContextProvider = ({ children }) => {
   const navigate = useNavigate();
   const [users, setUsers] = useState([]);
+  const [user, setUser] = useState([]);
   const [isAuthenticated, setIsAuthenticated] = useState(false);
 
   const [authTokens, setAuthTokens] = useState(() =>
@@ -31,7 +32,6 @@ export const ContextProvider = ({ children }) => {
         body: JSON.stringify(data),
         credentials: "include",
       });
-
       if (!response.ok) {
         throw new Error(`HTTP error! status: ${response.status}`);
       }
@@ -47,7 +47,10 @@ export const ContextProvider = ({ children }) => {
 
   const loginUser = async (login_data) => {
     try {
-      const response = await fetch(urlGlobal + "login/", {
+      const url = urlGlobal + "login/";
+      console.log(url);
+
+      const response = await fetch(url, {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
@@ -67,7 +70,6 @@ export const ContextProvider = ({ children }) => {
         setIsAuthenticated(true);
         localStorage.setItem("access", JSON.stringify(data.access));
         localStorage.setItem("refresh", JSON.stringify(data.refresh));
-        localStorage.setItem("rol", JSON.stringify(data.user_type));
         navigate("/");
         console.log("Login Success");
       } else {
@@ -78,18 +80,52 @@ export const ContextProvider = ({ children }) => {
     }
   };
 
+//   const getUsers = async () => {
+//       try {
+//         const url = urlGlobal + "all-users/";
+//       const response = await axios.get(url);
+//       if (response.statusText === "OK") {
+//         if (users.length === 0) {
+//           setUsers(response.data);
+//         }
+//       } else {
+//         console.log(response.error);
+//       }
+//     } catch (error) {
+//       console.error("Error", error);
+//     }
+//   };
+
   const getUsers = async () => {
     try {
-      const response = await axios.get("");
-      if (response.statusText === "OK") {
-        if (users.length === 0) {
-          setUsers(response.data);
-        }
+      const url =  urlGlobal + "all-users/"; // Ensure urlGlobal is correctly initialized in context.jsx
+      const response = await axios.get(url);
+      
+      if (response.status === 200) { // Use response.status instead of statusText for better reliability
+        setUsers(response.data); // Directly set the data
+        return response;
       } else {
-        console.log(response.error);
+        console.error("Error fetching users:", response);
       }
     } catch (error) {
-      console.error("Error", error);
+      console.error("Error fetching users:", error); // Improved error logging
+    }
+  };
+
+  const getUserById = async (id) => {
+    try {
+      const url =  urlGlobal + "user/" + id + "/"; 
+      const response = await axios.get(url);
+      
+      if (response.status === 200) { 
+        setUser(response.data); 
+        console.log(response);
+        return response;  //return data
+      } else {
+        console.error("Error fetching users:", response);
+      }
+    } catch (error) {
+      console.error("Error fetching users:", error); 
     }
   };
 
@@ -98,11 +134,11 @@ export const ContextProvider = ({ children }) => {
     localStorage.clear();
     navigate("/");
   };
-  //for register new user from Admin dashboard
+  
+  //to register new user from Admin dashboard
   const registerUserAdmin = async (data) => {
     try {
-      const url =
-        "https://h3-20-proptech-production.up.railway.app/api/register/";
+      const url = urlGlobal + "register/";
 
       const response = await fetch(url, {
         method: "POST",
@@ -121,7 +157,36 @@ export const ContextProvider = ({ children }) => {
 
       const result = await response.json();
       console.log("Registration successful:", result);
-      //   navigate("/login");
+
+      return result;
+    } catch (error) {
+      console.error("Registration error:", error.message);
+      throw error;
+    }
+  };
+
+//to update user information 
+const updateUserInformation = async (data) => {
+    try {
+      const url = urlGlobal + "update-user-information/";
+
+      const response = await fetch(url, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Accept: "application/json",
+        },
+        body: JSON.stringify(data),
+        credentials: "include",
+      });
+      console.log("Before SHOW:::", response);
+
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
+
+      const result = await response.json();
+      console.log("Registration successful:", result);
       return result;
     } catch (error) {
       console.error("Registration error:", error.message);
@@ -135,12 +200,14 @@ export const ContextProvider = ({ children }) => {
         setUsers,
         users,
         getUsers,
+        getUserById,
         authTokens,
         setAuthTokens,
         loginUser,
         logoutUser,
         registerUser,
         registerUserAdmin,
+        updateUserInformation,
         isAuthenticated,
       }}
     >
