9406ab6d22a9fdab76e1e0ef565aa6fde2281d8b
Juan Novas <juanignacionovas@gmail.com>
2024-12-13 10:36:30 -0300

    swagger update

--- server/users/views.py
+++ server/users/views.py
@@ -247,7 +247,19 @@ class UserDetailView(generics.RetrieveAPIView):
     
 class UpdateUserStatusView(APIView):
     permission_classes = [IsAuthenticated]
-    
+    @swagger_auto_schema(
+        operation_summary="Actualizar información del usuario",
+        operation_description="Permite actualizar la información personal y documentos del usuario autenticado.",
+        manual_parameters=[
+            openapi.Parameter(
+                'user_type', openapi.IN_FORM, 
+                description="New type of user (Admin, User, Investor, Borrower)", type=openapi.TYPE_STRING, required=True
+            ),
+        ],
+        responses={
+            400: openapi.Response("Errores en los datos enviados"),
+        }
+    )
     def post(self, request, user_id, *args, **kwargs):
         if not request.user.is_staff:
             return Response(
