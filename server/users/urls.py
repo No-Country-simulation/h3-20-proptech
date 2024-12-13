a7ee5eb3722a42b557327adeff7b53303cb3ff65
Juan Novas <juanignacionovas@gmail.com>
2024-12-13 10:31:15 -0300

    Update status endpoint added

--- server/users/urls.py
+++ server/users/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from rest_framework_simplejwt.views import TokenRefreshView
-from .views import RegisterView, RegisterWithKYC, ValidateUserView, UpdatePersonalDataView, AddGarantorView, CustomTokenObtainPairView, UpdateUserInformationView, UserListView, UserDetailView
+from .views import RegisterView, RegisterWithKYC, ValidateUserView, UpdatePersonalDataView, AddGarantorView, CustomTokenObtainPairView, UpdateUserInformationView, UserListView, UserDetailView, UpdateUserStatusView
 
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
@@ -12,5 +12,6 @@ urlpatterns = [
     path('addgarantor/', AddGarantorView.as_view(), name='add_garantor'),
     path('update-user-information/', UpdateUserInformationView.as_view(), name='update_user_information'),
     path('all-users/', UserListView.as_view(), name='all_users'),
-    path('user/<int:pk>/', UserDetailView.as_view(), name='user')
+    path('user/<int:pk>/', UserDetailView.as_view(), name='user'),
+    path('update-status/<int:user_id>/', UpdateUserStatusView.as_view(), name='update_status'),
 ]
